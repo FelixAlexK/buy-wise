20cde167c3fb1ba8ce89636cdbbed98560cbc2d8
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-09-30 03:06:46 +0200

    fix: adjust margin and max-width for PurchasePage form layout

--- src/pages/purchase.page.tsx
+++ src/pages/purchase.page.tsx
@@ -1,6 +1,6 @@
 export default function PurchasePage() {
     return (
-        <form action="" className="flex flex-col mt-36">
+        <form action="" className="flex flex-col mt-18 max-w-md mx-auto">
             <label className="mx-auto mb-10 text-2xl font-bold"  htmlFor="purchase-input">PURCHASE PRICE</label>
             <input className="border px-8 py-2 mb-8 rounded-md text-center" id="purchase-input" type="text" placeholder="50$" />
             <button className="bg-black text-white px-8 py-2 rounded-md max-w-1/2 mx-auto" type="submit">Submit</button>
