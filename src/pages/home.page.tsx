21a0e3ea1a5b24bb0391086a76269c6315286f44
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-09-30 03:06:42 +0200

    feat: update Home component to display Card components with saved values

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,3 +1,11 @@
+import Card from "../components/card.component";
+
 export default function Home() {
-  return <div>Home Page</div>;
+    return (
+        <div className="w-full max-w-md mx-auto grid gap-8 mt-18">
+            
+            <Card title="Money Saved" value="2323434€" />
+            <Card title="Work Time Saved" value="1 Week, 4 days" />
+      </div>
+  )
 }
