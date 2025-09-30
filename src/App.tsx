f6db1782f3150dcba46afc19ad12037d035944ae
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-09-30 02:49:03 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router"
+import Nav from "./components/mobile-nav.component"
+
+
+function App() {
+  
+
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <main className="  mx-auto p-8">
+        <Outlet />
+      </main>
+      
+      <footer className="mt-auto"><Nav /></footer>
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import Home from './pages/home.page.tsx'
+
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import Nav from './components/mobile-nav.component.tsx'
+import PurchasePage from './pages/purchase.page.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [
+      {
+        path: "home", 
+        Component: Home,
+      },
+      {
+        path: "purchase",
+        Component: PurchasePage,
+      },
+      {
+        path: "settings",
+        Component: () => <div>Settings Page</div>,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    
+      <RouterProvider router={router} />
+    
+  </StrictMode>,
+)
