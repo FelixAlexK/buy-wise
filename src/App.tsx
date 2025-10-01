d6ad57e45647b3dcf58593f2e8cf7ffc6543eb1d
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-02 00:13:35 +0200

    feat: implement modal and routing structure with desktop and mobile navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,43 @@
 import { Outlet } from 'react-router'
-import Nav from './components/mobile-nav.component'
+import MobileNavbar from './components/mobile-nav.component'
+import { DesktopNavbar } from './components/desktop-nav.component'
+import Modal from './components/modal.component'
+import { useState, useEffect } from 'react';
+import Settings from './pages/settings.page';
 
 function App() {
+  const [showModal, setShowModal] = useState(() => {
+    const saved = localStorage.getItem('showModal');
+
+    if (saved === null) {
+      localStorage.setItem('showModal', '0');
+    }
+
+    if (saved === '0') {
+      return true;
+    }
+
+    return false;
+  });
+
+  const close = () => {
+    setShowModal(false);
+    localStorage.setItem('showModal', '1');
+  }
+  
   return (
     <div className="flex flex-col min-h-screen ">
-      <main className="   p-8">
-        <Outlet />
+      <header className='lg:hidden'>
+        <h1 className="text-3xl font-bold text-center p-4 border-b">BuyWise</h1>
+      </header>
+      <main className="p-8">
+        <Modal isOpen={showModal} onClose={() => close()} >
+          <Settings />
+        </Modal>
+        <DesktopNavbar content={ <Outlet /> } ></DesktopNavbar>
       </main>
 
-      <footer className="mt-auto"><Nav /></footer>
+      <footer className="mt-auto lg:hidden"><MobileNavbar /></footer>
     </div>
   )
 }
