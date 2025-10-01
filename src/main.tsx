e3ff022cfce9a89e5d4ed65a6c30da068e7b35a8
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-02 00:13:47 +0200

    feat: implement desktop and mobile navigation components with routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,33 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
-import App from './App.tsx'
-
-import Home from './pages/home.page.tsx'
-import PurchasePage from './pages/purchase.page.tsx'
 import './index.css'
+import { router } from './routes.ts'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: App,
-    children: [
-      {
-        path: 'home',
-        Component: Home,
-      },
-      {
-        path: 'purchase',
-        Component: PurchasePage,
-      },
-      {
-        path: 'settings',
-        Component: () => <div>Settings Page</div>,
-      },
-    ],
-  },
-])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
