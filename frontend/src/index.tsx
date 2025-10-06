8c3bf55428ed9f31d3771856d08cfb4bccbcb352
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-10-06 19:35:16 +0200

    feat: add initial setup for frontend application with routing and query management

---
+++ frontend/src/index.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router/dom'
+import { router } from './routes.ts'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './utils/orpc'
+
+import './index.css'
+
+const rootElement = document.getElementById('root')
+createRoot(rootElement!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  </StrictMode>,
+)
