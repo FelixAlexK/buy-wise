d030bfca4605b8ea0baf4f347f23cf721b40a756
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-10-06 19:35:05 +0200

    feat: enhance desktop navbar with login status display and navigation link

--- frontend/src/components/desktop-nav.component.tsx
+++ frontend/src/components/desktop-nav.component.tsx
@@ -1,7 +1,12 @@
 import { Banknote, Home, Settings } from 'lucide-react'
 import { NavLink } from 'react-router'
+import { authClient } from '../lib/auth-client';
 
 export function DesktopNavbar({ content }: { content: React.ReactNode }) {
+
+  const { data } = authClient.useSession();
+
+
   return (
     <>
       <nav className="hidden lg:block fixed h-full w-64 z-10 top-0 left-0 overflow-x-hidden pr-4 py-4 border-r">
@@ -40,7 +45,30 @@ export function DesktopNavbar({ content }: { content: React.ReactNode }) {
             <span className="mr-2 text-lg ">Settings</span>
             <Settings />
           </NavLink>
+          <NavLink
+            to="/login"
+            className={({ isActive }) =>
+              [
+                'p-4 w-full flex items-center justify-center rounded-r-md transition-colors duration-200',
+                isActive ? 'bg-black text-white font-semibold' : 'hover:bg-gray-200',
+              ].join(' ')}
+          >
+            <span className="mr-2 text-lg ">Login</span>
+            <Settings />
+          </NavLink>
         </div>
+            <div className=" ">
+            {data ? (
+              <div className="mt-4 text-center">
+              <span className="text-sm">Signed in as</span>
+              <p className="font-medium">{data.user?.email}</p>
+              </div>
+            ) : (
+              <div className="mt-4 text-center">
+              <span className="text-sm">Not signed in</span>
+              </div>
+            )}
+            </div>
 
       </nav>
       <div className="lg:ml-64">
