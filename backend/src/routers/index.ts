c98bd388f9aecfe6f5608c9c99647394db61ff32
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-10-07 21:01:11 +0200

    refactor: reorganize imports and structure of appRouter in index.ts

--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -1,18 +1,22 @@
-import { protectedProcedure, publicProcedure } from "../lib/orpc";
-import type { RouterClient } from "@orpc/server";
-import { purchaseRouter } from "./purchase";
+import type { RouterClient } from '@orpc/server'
+import { protectedProcedure, publicProcedure } from '../lib/orpc'
+import { purchaseRouter } from './purchase'
+import { settingRouter } from './settings'
+import { statRouter } from './stat'
 
 export const appRouter = {
-    healthCheck: publicProcedure.handler(() => {
-        return "OK";
-    }),
-    privateData: protectedProcedure.handler(({ context }) => {
-        return {
-            message: "This is private",
-            user: context.session?.user,
-        };
-    }),
-    purchase: purchaseRouter,
-};
-export type AppRouter = typeof appRouter;
-export type AppRouterClient = RouterClient<typeof appRouter>;
+  healthCheck: publicProcedure.handler(() => {
+    return 'OK'
+  }),
+  privateData: protectedProcedure.handler(({ context }) => {
+    return {
+      message: 'This is private',
+      user: context.session?.user,
+    }
+  }),
+  purchase: purchaseRouter,
+  stat: statRouter,
+  setting: settingRouter,
+}
+export type AppRouter = typeof appRouter
+export type AppRouterClient = RouterClient<typeof appRouter>
