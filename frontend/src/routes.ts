7ea260c205f939afbc7e18fb0f86e86def1ff682
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-10-07 21:52:38 +0200

    fix: correct import path for Login component in routes

--- frontend/src/routes.ts
+++ frontend/src/routes.ts
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router'
 import App from './App'
 import Home from './pages/home.page'
-import Login from './pages/login'
+import Login from './pages/login.page'
 import PurchasePage from './pages/purchase.page'
 import Settings from './pages/settings.page'
 
