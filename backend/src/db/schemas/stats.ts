7ae50063470795661902d1f0f3cbf466384faf70
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2025-10-07 21:00:50 +0200

    fix: adjust default and not null constraints for createdAt and updatedAt fields in stats table

--- backend/src/db/schemas/stats.ts
+++ backend/src/db/schemas/stats.ts
@@ -7,12 +7,11 @@ export const stat = sqliteTable('stat', {
   workTimeSaved: numeric('work_time_saved', { mode: 'number' }).notNull().default(0),
   userId: text('user_id').notNull().references(() => user.id, { onDelete: 'cascade' }),
   createdAt: integer('created_at', { mode: 'timestamp_ms' })
-    .$defaultFn(() => new Date())
-    .notNull(),
+    .$defaultFn(() => new Date()),
+
   updatedAt: integer('updated_at', { mode: 'timestamp_ms' })
     .$defaultFn(() => new Date())
-    .$onUpdate(() => new Date())
-    .notNull(),
+    .$onUpdate(() => new Date()),
 }, table => [
   index('work_time_saved_idx').on(table.workTimeSaved),
   index('money_saved_idx').on(table.moneySaved),
