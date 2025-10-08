4bd461e0eceb59150b74294dec567f7c69710962
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-08 22:15:46 +0200

    feat: add WorthItModal component for purchase decision confirmation

---
+++ frontend/src/components/worthit-modal.component.tsx
@@ -0,0 +1,30 @@
+import { timeAtWork } from "../utils/calculations"
+import Modal from "./modal.component"
+
+export default function WorthItModal({ isOpen, salary, workingTime, value, handleDontBuy, handleBuy }: { isOpen: boolean, salary: number, workingTime: number, value: number, handleDontBuy: () => void, handleBuy: () => void }) {
+    if (!isOpen) return null
+    return (
+        <Modal isOpen={isOpen}>
+            <div className="text-center">
+                <h2 className="text-2xl font-bold mb-4">Worth it?</h2>
+                <p className="mb-4">Time at Work</p>
+                <p className="text-lg font-semibold">{timeAtWork(salary, workingTime, value)} hours</p>
+                <div className='gap-4 flex justify-center'>
+                    <button
+                        className="bg-black text-white mt-8 px-8 py-2 rounded-md"
+                        onClick={handleDontBuy}
+                    >
+                        Don't Buy
+                    </button>
+                    <button
+                        className="bg-black text-white mt-8 px-8 py-2 rounded-md"
+                        onClick={handleBuy}
+                    >
+                        Buy
+                    </button>
+                </div>
+            </div>
+        </Modal>
+    )
+}
+
