c1c2156efaaf5ed1fef491089e4f27499b49eb31
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-08 23:24:21 +0200

    fix: update basePath in ESLint configuration for consistency

--- eslint.config.js
+++ eslint.config.js
@@ -5,18 +5,17 @@ export default antfu({
   react: true,
   typescript: true,
   jsx: true,
-  basePath: __dirname,
+  basePath: '.',
   formatters: true,
   jsonc: true,
   ignores:
     ['dist/*', 'node_modules/*', 'public/*'],
-  
 
   rules: {
     'react/no-comment-textnodes': 'off',
     'react-hooks-extra/no-unnecessary-use-prefix': 'off',
     'react-hooks-extra/prefer-use-state-lazy-initialization': 'off',
-    "unused-imports/no-unused-vars": 'warn',
+    'unused-imports/no-unused-vars': 'warn',
     'node/prefer-global/process': 'off',
   },
 })
