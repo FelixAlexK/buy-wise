de9c47f8c936e3ce7191324a5db66ff892ac7f19
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-08 23:24:01 +0200

    feat: add loading state to Modal component

--- frontend/src/components/modal.component.tsx
+++ frontend/src/components/modal.component.tsx
@@ -1,10 +1,19 @@
-export default function Modal({ isOpen, children }: { isOpen: boolean, children: React.ReactNode }) {
+import { Loader } from 'lucide-react'
+
+export default function Modal({ isOpen, children, isLoading }: { isOpen: boolean, children: React.ReactNode, isLoading: boolean }) {
   if (!isOpen)
     return null
+
   return (
-    <div className="fixed inset-0 bg-black/90 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-11/12 max-w-md">
-        {children}
+    <div className="fixed  inset-0 bg-black/90 bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white lg:ml-64 p-6 rounded-lg shadow-lg w-11/12 max-w-md">
+        {isLoading
+          ? (
+              <Loader className="animate-spin text-center w-full" />
+            )
+          : (
+              children
+            )}
       </div>
     </div>
   )
