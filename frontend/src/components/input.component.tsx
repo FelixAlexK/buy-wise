948460798a2f2a1f827e24aaa6c3eae6b760701c
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-09 02:23:08 +0200

    feat: refactor InputComponent to use UI components for improved styling and structure

--- frontend/src/components/input.component.tsx
+++ frontend/src/components/input.component.tsx
@@ -1,10 +1,12 @@
 import type React from 'react'
+import { Input } from '../components/ui/input'
+import { Label } from '../components/ui/label'
 
-export default function InputComponent({ label, placeholder, type = 'text', required = false, onChange, name, value, onBlur }: { label?: string, placeholder?: string, type?: React.HTMLInputTypeAttribute, required?: boolean, onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void, name: string, value?: string, onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void }) {
+export default function InputComponent({ label, placeholder, type = 'text', required = false, onChange, name, value, onBlur, min, step }: { label?: string, placeholder?: string, type?: React.HTMLInputTypeAttribute, required?: boolean, onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void, name: string, value?: string, onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void, min?: number, step?: number }) {
   return (
-    <div className="flex flex-col">
-      {label && <label className="font-semibold mb-2" htmlFor={name}>{label}</label>}
-      <input required={required} name={name} className="border px-8 py-2 rounded-md text-center" id={name} type={type} value={value} placeholder={placeholder} onChange={onChange} />
+    <div className="grid gap-2">
+      {label && <Label htmlFor={name}>{label}</Label>}
+      <Input required={required} name={name} id={name} type={type} value={value} placeholder={placeholder} onChange={onChange} min={min} step={step} />
     </div>
   )
 }
