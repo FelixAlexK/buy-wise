65fdc3007fc583ba585a794faa39031c86cc16ea
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-09 02:23:40 +0200

    feat: refactor Home component for improved structure and styling using UI components

--- frontend/src/pages/home.page.tsx
+++ frontend/src/pages/home.page.tsx
@@ -1,12 +1,17 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
-import Card from '../components/card.component'
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { Item, ItemActions, ItemContent, ItemDescription, ItemTitle } from '@/components/ui/item'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Skeleton } from '@/components/ui/skeleton'
 import { authClient } from '../lib/auth-client'
 import { orpc } from '../utils/orpc'
 
 export default function Home() {
   const { data } = authClient.useSession()
-  const purchases = useQuery(orpc.purchase.getAll.queryOptions({ enabled: !!data?.user?.id }))
-  const stats = useQuery(orpc.stat.getById.queryOptions({ input: { userId: data?.user.id! }, enabled: !!data?.user?.id }))
+  const purchases = useQuery(orpc.purchase.getAll.queryOptions({ input: { userId: data?.user.id ?? '' }, enabled: !!data?.user?.id }))
+  const stats = useQuery(orpc.stat.getById.queryOptions({ input: { userId: data?.user.id ?? '' }, enabled: !!data?.user?.id }))
 
   const purchaseDeleteMutation = useMutation(orpc.purchase.delete.mutationOptions({
     onSuccess: () => {
@@ -19,26 +24,79 @@ export default function Home() {
   }
 
   return (
-    <div className="w-full max-w-md mx-auto grid gap-8 mt-18">
-      <Card title="Money Saved" value={`${stats.data?.moneySaved ?? '--'} €`} />
+    <div className="w-full max-w-sm mx-auto grid gap-8 mt-18">
 
-      <Card title="Work Time Saved" value={`${stats.data?.workTimeSaved ?? '--'} hours`} />
-      <ul className="space-y-4">
-        {purchases.data?.map(purchase => (
-          <li className="border border-gray-300 p-4 rounded-md w-full " key={purchase.id}>
-            <div className="flex justify-between items-center">
-              <time className="text-gray-500 text-sm" dateTime={purchase.createdAt?.toISOString()}>{new Date(purchase.createdAt ?? '-').toLocaleString()}</time>
-              <span className="">Value:</span>
-              {' '}
-              {purchase.value}
-              €
-              {data?.user && (
-                <button onClick={() => handleDeletePurchase(purchase.id)} className="bg-red-500 text-white px-2 py-1 rounded ml-4">Delete</button>
+      <Card>
+        <CardHeader>
+          <CardTitle>Money Saved</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {stats.isLoading
+            ? <Skeleton className="h-12 rounded-xl" />
+
+            : (
+                <span className="text-4xl font-bold">
+                  {`${stats.data?.moneySaved ?? '--'} €`}
+                </span>
               )}
+
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Work Time Saved</CardTitle>
+        </CardHeader>
+        <CardContent>
+          {stats.isLoading
+            ? <Skeleton className="h-12 rounded-xl" />
+
+            : (
+                <span className="text-4xl font-bold">
+
+                  {`${stats.data?.workTimeSaved ?? '--'} hours`}
+                </span>
+              )}
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            Last Purchases (
+            {purchases.data?.length ?? 0}
+            )
+          </CardTitle>
+          <CardDescription>Most recent purchases are shown first</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <ScrollArea className="h-full max-h-80">
+            <div>
+              {purchases.isLoading
+                ? <Skeleton className="h-80 rounded-xl" />
+                : purchases.data?.map(purchase => (
+                    <React.Fragment key={purchase.id}>
+                      <Item variant="outline" className="mb-4">
+                        <ItemContent>
+                          <ItemTitle>
+                            {purchase.value}
+                            €
+                          </ItemTitle>
+                          <ItemDescription>{new Date(purchase.createdAt!).toLocaleString()}</ItemDescription>
+                        </ItemContent>
+                        <ItemActions>
+                          <Button variant="destructive" onClick={() => handleDeletePurchase(purchase.id)}>Delete</Button>
+                        </ItemActions>
+
+                      </Item>
+                    </React.Fragment>
+
+                  ))}
             </div>
-          </li>
-        ))}
-      </ul>
+
+          </ScrollArea>
+        </CardContent>
+      </Card>
+
     </div>
   )
 }
