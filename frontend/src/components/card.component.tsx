9bfa46aacdd57a0c75093244da71851bc8547533
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-09 02:24:22 +0200

    feat: remove Card component to streamline codebase and improve maintainability

--- frontend/src/components/card.component.tsx
+++
@@ -1,9 +0,0 @@
-export default function Card({ title, value, children }: { title?: string, value?: string, children?: React.ReactNode }) {
-  return (
-    <div className="border px-8 py-4 rounded-md shadow-md max-w-md w-full">
-      {title && <h1 className="text-sm text-gray-500 font-semibold">{title}</h1>}
-      {value && <p className="mt-4 font-bold text-4xl text-center">{value}</p>}
-      {children}
-    </div>
-  )
-}
