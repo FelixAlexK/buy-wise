dc1ba249f86838b39dc9d7b18cbf4cb04de86067
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-09 19:07:38 +0200

    feat: enhance WorthItModal to display daily working hours and format time at work

--- frontend/src/components/worthit-modal.component.tsx
+++ frontend/src/components/worthit-modal.component.tsx
@@ -1,18 +1,22 @@
-import { timeAtWork } from '../utils/calculations'
+import { convertHoursToReadableFormat, timeAtWork } from '../utils/calculations'
 import Modal from './modal.component'
 import { Button } from './ui/button'
 
-export default function WorthItModal({ isOpen, salary, workingTime, value, handleDontBuy, handleBuy, onClose }: { isOpen: boolean, salary: number, workingTime: number, value: number, handleDontBuy: () => void, handleBuy: () => void, onClose: () => void }) {
+export default function WorthItModal({ isOpen, salary, workingTime, dailyWorkingHours, value, handleDontBuy, handleBuy, onClose }: { isOpen: boolean, salary: number, workingTime: number, dailyWorkingHours: number, value: number, handleDontBuy: () => void, handleBuy: () => void, onClose: () => void }) {
   if (!isOpen)
     return null
   return (
     <Modal onClose={onClose} title="Worth it?" description="Evaluate the worth of this purchase" isOpen={isOpen}>
       <div className="flex flex-col">
-        <p className="text-lg text-gray-600 mb-2">Time at Work</p>
+        <p className="text-lg text-gray-600 mb-2">
+          Time at Work (
+          {dailyWorkingHours}
+          h/day):
+        </p>
         <p className="text-2xl font-semibold mb-8">
-          {timeAtWork(salary, workingTime, value)}
-          {' '}
-          hours
+
+          {convertHoursToReadableFormat(timeAtWork(salary, workingTime, value), dailyWorkingHours)}
+
         </p>
         <div className="flex flex-row gap-4">
           <Button
