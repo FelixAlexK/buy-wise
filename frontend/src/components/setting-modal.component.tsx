a63f5aab488698e7a43589c6c6faa34af2c5761d
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-09 02:23:23 +0200

    feat: refactor SettingModal component for improved structure and styling

--- frontend/src/components/setting-modal.component.tsx
+++ frontend/src/components/setting-modal.component.tsx
@@ -1,25 +1,29 @@
-import InputComponent from "./input.component"
-import Modal from "./modal.component"
+import InputComponent from './input.component'
+import Modal from './modal.component'
+import { Button } from './ui/button'
+import { Spinner } from './ui/spinner'
 
 export default function SettingModal({ isOpen, onClose, formData, handleChange, handleSettingCreation, isLoading }: { isOpen: boolean, onClose: () => void, formData: any, handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void, handleSettingCreation: () => Promise<void>, isLoading: boolean }) {
-    if (!isOpen) return null
-    return (
-        <Modal isLoading={isLoading} isOpen={isOpen}>
-                <form
-                  onSubmit={async (e) => {
-                    e.preventDefault()
-        
-                    handleSettingCreation()
-                    onClose()
-                    
-                  }}
-                  className="space-y-4 "
-                >
-        
-                  <InputComponent value={formData.monthlySalary} onChange={handleChange} name="monthlySalary" required label="Monatliches Gehalt" placeholder="50€" />
-                  <InputComponent value={formData.weeklyHours} onChange={handleChange} name="weeklyHours" required label="Wöchentliches Arbeitsstunden" placeholder="40 hours/week" />
-                  <button className="bg-black text-white mt-8 px-8 py-2 rounded-md w-full" type="submit">Save</button>
-                </form>
-                </Modal>
-    )
+  if (!isOpen)
+    return null
+  return (
+    <Modal onClose={onClose} title="Settings" description="Configure your settings" isOpen={isOpen}>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault()
+
+          handleSettingCreation()
+          onClose()
+        }}
+      >
+        <div className="flex flex-col gap-6">
+
+          <InputComponent value={formData.monthlySalary} onChange={handleChange} name="monthlySalary" required label="Monatliches Gehalt" placeholder="50€" />
+          <InputComponent value={formData.weeklyHours} onChange={handleChange} name="weeklyHours" required label="Wöchentliches Arbeitsstunden" placeholder="40 hours/week" />
+          <Button type="submit">{isLoading ? <Spinner /> : 'Save'}</Button>
+        </div>
+      </form>
+
+    </Modal>
+  )
 }
