0d3de74dbb8f30fd1b430a26a4601170376bb25e
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-09 22:31:55 +0200

    refactor: simplify App component by removing unused header and footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,13 @@
 import { Outlet } from 'react-router'
-import { DesktopNavbar } from './components/desktop-nav.component'
-import MobileNavbar from './components/mobile-nav.component'
+import { DesktopNavbar } from './components/nav.component'
+import { Toaster } from './components/ui/sonner'
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen ">
-      <header className="lg:hidden">
-        <h1 className="text-3xl font-bold text-center p-4 border-b">BuyWise</h1>
-      </header>
-      <main className="">
 
-        <DesktopNavbar content={<Outlet />}></DesktopNavbar>
-      </main>
-
-      <footer className="mt-auto lg:hidden"><MobileNavbar /></footer>
+      <DesktopNavbar content={<Outlet />}></DesktopNavbar>
+      <Toaster position="top-right" />
     </div>
   )
 }
