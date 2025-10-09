008fea1bfb893d611b19f9c87b1b635013ffd001
Felix Kuhbier <fkuhbier@stud.hs-heilbronn.de>
2025-10-10 01:26:06 +0200

    feat: wrap Outlet in Suspense for improved loading experience in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from 'react'
 import { Outlet } from 'react-router'
 import { DesktopNavbar } from './components/nav.component'
 import { Toaster } from './components/ui/sonner'
+import { Spinner } from './components/ui/spinner'
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen ">
 
-      <DesktopNavbar content={<Outlet />}></DesktopNavbar>
+      <DesktopNavbar>
+        <Suspense fallback={<Spinner />}>
+          <Outlet />
+        </Suspense>
+      </DesktopNavbar>
       <Toaster position="top-right" />
     </div>
   )
